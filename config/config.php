c0af25c13e39b2426757fb20f2439b458c6b34d4
Lucas <l.przezdziek@gmail.com>
2021-03-30 21:31:30 +0200

    Update config.php

--- config/config.php
+++ config/config.php
@@ -4,10 +4,6 @@ $config = [];
 $config['displayErrorDetails'] = false;
 $config['addContentLengthHeader'] = false;
 
-/*$config['database_name'] = "onepro_cms";
-$config['server'] = "localhost";
-$config['username'] = "onepro_cms";
-$config['password'] = "WehikulCzasu2010";*/
 
 $config['database_name'] = "slim";
 $config['server'] = "localhost";
